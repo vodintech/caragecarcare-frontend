dc54fc78a5b0058fc7861df0a91b0d2f350bf1c1
bahugunasiddarth <bahugunasiddhi@gmail.com>
2025-05-14 14:44:05 +0530

    update

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -320,7 +320,7 @@ const ServicePage = () => {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
         <h3 className="mt-2 text-lg font-medium text-gray-900">No content available</h3>
-        <p className="mt-1 text-sm text-gray-500">We couldn't find any services to display.</p>
+        <p className="mt-1 text-sm text-gray-500">We couldn&apos;t find any services to display.</p>
       </div>
     );
   };
