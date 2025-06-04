073546faf7a30049493e35c329cf0af82f0b54a5
bahugunasiddarth <bahugunasiddhi@gmail.com>
2025-06-04 20:28:21 +0530

    update

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '@/components/Navbar';
 import Image from 'next/image';
 import { ChevronRight, ChevronDown, Check, Shield, Star, Circle, Trash2 } from 'lucide-react';
